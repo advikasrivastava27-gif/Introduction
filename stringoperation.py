2823fd5fceebbd05c4045352c46e5ca1d7c32418
advikasrivastava27-gif <advikasrivastava27@gmail.com>
2026-07-16 18:44:37 -0700

    Add files via upload

---
+++ stringoperation.py
@@ -0,0 +1,7 @@
+name= "Advika"
+print(len(name)) #length
+print(name [4])
+print(name [0:2])
+
+lname="Srivastava"
+print(name+" "+lname)
---
+++ typecasting.py
@@ -0,0 +1,7 @@
+num=22 #integer
+print(num)
+print(float(num))
+
+num1=88.9 #float
+print(num1)
+print(int(num1))
