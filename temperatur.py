044000c44a2e321698147cd64aec9493cfcaf2be
advikasrivastava27-gif <advikasrivastava27@gmail.com>
2026-07-23 15:31:39 -0700

    Add files via upload

---
+++ temperatur.py
@@ -0,0 +1,7 @@
+temp=int(input("enter temperature: "))
+if(temp>70):
+    print("temperature outside is warmer than 70 degrees")
+    print("it is suitable for wearing light clothes")
+else:
+    print("temperature is below 70 degrees")
+    print("it is not suitable for light clothes")
